77095c5a7e63e7d3ab517c9185d1b48af63cca0a
Shenja <shenja.chekan@gmail.com>
2021-12-29 16:44:52 +0300

    Начата работа с главное страницой, верстаю сразу под мобильные устройства, на главной странице готов header, и текстовое поле для поиска, а так же кнопка для поиска по фото

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <style>
+:root {
+  --main-bg-color: #1152fd;
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+h1 {
+  margin: 0;
+}
+.container {
+  max-width: 100vw;
+  height: 100vh;
+
+  margin: 0 auto;
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>Главная страница</h1>
+  <header class="header">
+    <div class="search">
+      <input
+        type="text"
+        class="search__input-text"
+        placeholder=""
+        id="search__input-text"
+      />
+      <label for="search__input-text" class="search__placeholder-input">
+        <div class="search__icon-placeholder">
+          <img
+            src="../assets/icon_search_input.png"
+            alt="icon search"
+            width="100%"
+          />
+        </div>
+        <p class="search__text-placeholder">Search</p>
+      </label>
+      <button class="search__btn-foto"></button>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -8,3 +28,81 @@ export default {
   props: {},
 };
 </script>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 231px;
+
+  padding: 53px 9.6% 0;
+
+  border-radius: 0 0 38px 38px;
+  background-color: var(--main-bg-color);
+}
+
+.search {
+  width: 100%;
+  height: 60px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  position: relative;
+}
+
+.search__input-text,
+.search__btn-foto {
+  height: 100%;
+
+  border: none;
+  outline: none;
+  border-radius: 14px;
+  background-color: #f7f8f9;
+}
+
+.search__input-text {
+  width: 224px;
+
+  padding-left: 18px;
+}
+
+.search__btn-foto {
+  width: 60px;
+
+  background-image: url(../assets/icon_search_btn.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 25px;
+}
+
+.search__placeholder-input {
+  display: flex;
+  margin-left: 18px;
+
+  position: absolute;
+
+  transition: all 0.2s linear;
+}
+
+.search__icon-placeholder {
+  width: 20px;
+  height: 20px;
+}
+.search__text-placeholder {
+  margin: 0;
+  margin-left: 6.2px;
+
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+
+  color: #d5dde0;
+}
+
+.search__input-text:focus ~ .search__placeholder-input {
+  display: none;
+}
+</style>
