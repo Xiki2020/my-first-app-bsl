ab4fef973fcf6f1e4b02a44ce78cb13ca3d547bb
Shenja <shenja.chekan@gmail.com>
2022-01-28 10:43:15 +0300

    может так

---
+++ src/Layouts/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="layout" />
+</template>
+
+<script>
+import AuthLayout from "./AuthLayout.vue";
+import MainLayout from "./MainLayout.vue";
+
+export default {
+  name: "Layout",
+
+  components: {
+    AuthLayout,
+    MainLayout,
+  },
+
+  computed: {
+    layout() {
+      return this.$route.meta.layout;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
